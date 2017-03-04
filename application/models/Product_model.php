049ffc717d5e21c00e589bee086d1d067117cfeb
mindziukasss <mindziukasss@gmail.com>
2017-03-04 08:27:20 +0200

    Added categories and popular products

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -17,4 +17,23 @@ class Product_model extends CI_Model{
     $query = $this->db->get();
     return $query->row();
   }
+
+  public function get_categories(){
+    $this->db->select('*');
+    $this->db->from('categories');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function get_popular(){
+    $this->db->select('P.*, COUNT(O.product_id) as total');
+    $this->db->from('orders AS O');
+    $this->db->join('products AS P', 'O.product_id = P.id', 'INNER');
+    $this->db->group_by('O.product_id');
+    $this->db->order_by('total', 'desc');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+
 }
